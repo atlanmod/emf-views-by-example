dee8934d25a51f898b9802404bf55913a34e8e14
James MIRANDA <james.pontes-miranda@imt-atlantique.fr>
2022-04-01 10:05:08 +0200

    Finish example of view navigation changing the filters

--- 5_View_Using_API/src/org/atlanmod/emfviews/example/NavigateViewChangeFilter.java
+++ 5_View_Using_API/src/org/atlanmod/emfviews/example/NavigateViewChangeFilter.java
@@ -11,6 +11,7 @@ import org.atlanmod.emfviews.virtuallinks.VirtualLinksPackage;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
@@ -21,7 +22,7 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
-public class NaviagteView {
+public class NavigateViewChangeFilter {
 	  static String root = new File("../").getAbsolutePath();	  
 
 	  static URI resourceURI(String relativePath) {
@@ -29,7 +30,7 @@ public class NaviagteView {
 	  }
 	  
 	  /**
-	   * Iterate over the a view to change its values and verify what happens with source models
+	   * Iterate over the view to change its filters values and verify what happens with source models
 	   * @param args
 	   * @throws IOException
 	   */
@@ -49,18 +50,26 @@ public class NaviagteView {
 	    EPackage.Registry.INSTANCE.put(Book.getNsURI(), Book);
 	    EPackage Publication = (EPackage) resSet.getResource(resourceURI("/Resources/metamodels/Basic/Publication.ecore"), true).getContents().get(0);
 	    EPackage.Registry.INSTANCE.put(Publication.getNsURI(), Publication);
+	    
+	    //Load the source model that is used in the view
+	    Resource book = resSet.getResource(resourceURI("/Resources/models/Basic/Book.xmi"), true);
 
-	    //Create EMF Resources
+	    //Create EMF Resources for the view
 	    Resource filteredInfoView  = resSet.getResource(resourceURI("/6_Sync_Filters/views/filteredInfo.eview"), true);
 	    filteredInfoView.load(null);
 	    
-	    //	    
+	    //print the view to check elements	    
 	    List <EObject> vElements = filteredInfoView.getContents();
 	    printView(vElements);
 	    
 	    //Try to adjust the ISBN values in the view
+	    adjustAllISBN(vElements);
 	    
+	    //print the view again to check the changes	    
+	    printView(vElements);
 	    
+	    //print the book to check if it changes
+	    printBook(book);	    
 	  }
 	  
 	  /**
@@ -89,17 +98,55 @@ public class NaviagteView {
 	  }
 	  
 	  /**
+	   * Navigate through the view to adjust the ISBN of its elements (when exists)
+	   * @param List <EObject> vElements
+	   */
+	  public static void adjustAllISBN(List <EObject> vElements) 
+	  {
+		  for (Iterator<EObject> iter = vElements.iterator() ; iter.hasNext();) {
+		    	EObject vElement = iter.next();
+			    adjustISBN(vElement);
+		  }
+	  }
+	  
+	  /**
 	   * Receive view object and change the ISBN value when appropriate
 	   * @param EObject object
 	   */
 	  public static void adjustISBN(EObject object) 
 	  {
 		  EStructuralFeature feature = object.eClass().getEStructuralFeature("ISBN");
-		  if (feature != null && feature.getEType() == EcorePackage.Literals.ESTRING) 
+		  if (feature != null) 
 		  {
 			  String isbn = (String) object.eGet(feature).toString();
 			  object.eSet(feature, "ISBN=" + isbn);
 		  }
+	  }
+	  
+	  /**
+	   * 
+	   * @param Resource book
+	   */
+	  public static void printBook(Resource book) 
+	  {
+		  List <EObject> bookElements = book.getContents();
+		  for (Iterator<EObject> iter = bookElements.iterator() ; iter.hasNext();) {
+		    	EObject bookElement = iter.next();
+			    EClass bookModelClass = bookElement.eClass();
+			    System.out.println(bookModelClass.getName());
+			    for (Iterator<EAttribute> iterAttr = bookModelClass.getEAllAttributes().iterator() ; iterAttr.hasNext();) {
+			    	EAttribute bookElementAttribute = (EAttribute) iterAttr.next();
+			    	
+			    	Object elementAttributeValue = bookElement.eGet(bookElementAttribute);
+			    	String attrName = bookElementAttribute.getName();
+			    	System.out.println(" " + attrName + ": " + elementAttributeValue);
+			    	if (bookElement.eIsSet(bookElementAttribute)) {
+			    		System.out.println();
+			    	} else {
+			    		System.out.println(" (default)");
+			    	}
+			    }
+		    }
 	  }
 
 }
